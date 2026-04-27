94a5f654d3cce8395b212c49f418ec65766c2200
kawther <likawther17@gmail.com>
2026-04-27 11:53:47 +0400

    Print the message line, centered with side borders

--- untitled1/src/Message.java
+++ untitled1/src/Message.java
@@ -2,5 +2,6 @@ public class Message {
     public static void main(String[] args) {
         System.out.println("********************");
         System.out.println("* *");
+        System.out.println("* \"Keep Learning\" *");
 }
     }
