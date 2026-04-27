a8f49886f600fa2a47cd99ad57d6e14082fa2566
kawther <likawther17@gmail.com>
2026-04-27 11:39:34 +0400

    print line 3

--- untitled1/src/Staircase.java
+++ untitled1/src/Staircase.java
@@ -2,6 +2,7 @@ public class Staircase {
     public static void main(String[] args) {
         System.out.println("#");//fist line
         System.out.println("##"); // 2 line
+        System.out.println("###"); // 3 line
 
     }
 }
