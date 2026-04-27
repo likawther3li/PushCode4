bdfc13ba424a2b83f90654883351b1c6e41312e7
kawther <likawther17@gmail.com>
2026-04-27 11:33:45 +0400

    Implement staircase pattern using multiple print statements without loops

---
+++ untitled1/src/Staircase.java
@@ -0,0 +1,10 @@
+public class Staircase {
+    public static void main(String[] args) {
+        System.out.println("#");
+        System.out.println("##");
+        System.out.println("###");
+        System.out.println("####");
+        System.out.println("#####");
+    }
+}
+
