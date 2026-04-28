614bb22805d77bd622c14b9cc742291d9eb099a2
kawther <likawther17@gmail.com>
2026-04-28 08:36:00 +0400

    Loop to print numbers in descending order from i-1 to 1

--- untitled1/src/loop/Loops.java
+++ untitled1/src/loop/Loops.java
@@ -6,7 +6,11 @@ public static void main(String[] args) {
     int lines = 4;
 
     for (int j = 1; j <= i; j++) {
+
         System.out.print(j);
+
+        for (int j = i - 1; j >= 1; j--) {
+            System.out.print(j);
     }
 }
 }
