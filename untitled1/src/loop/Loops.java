a4ae04e30c46c7740755e6a7b5ca5e814e656b3a
kawther <likawther17@gmail.com>
2026-04-28 08:34:37 +0400

    Loop to print numbers in ascending order from 1 to i

--- untitled1/src/loop/Loops.java
+++ untitled1/src/loop/Loops.java
@@ -2,6 +2,11 @@ package loop;
 
 public class Loops {
 public static void main(String[] args) {
+
     int lines = 4;
+
+    for (int j = 1; j <= i; j++) {
+        System.out.print(j);
+    }
 }
 }
